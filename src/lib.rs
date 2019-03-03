7196f5787875d788c11678cd32fc1f05d56f6029
Noah Lidell <noahlidell@gmail.com>
2019-03-03 03:02:45 -0600

    working processing of tvs into proper types

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,12 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
+#[derive(Debug,Deserialize)]
+struct Record {
+    input: usize,
+    output: u8,
+    err_msg: String,
+}
 
 
 mod fxn;
@@ -14,22 +20,14 @@ mod tests {
     use std::error::Error;
     use csv::Reader;
     use super::fxn;
-    use serde::Deserialize;
-    use std::io::prelude::*;
-    use std::path::Path;
 
     use std::env;
     use std::ffi::OsString;
     use std::fs::File;
     use std::process;
+
     // By default, struct field names are deserialized based on the position of
     // a corresponding field in the CSV data's header record.
-    #[derive(Debug,Deserialize)]
-    pub struct Record {
-        pub input: usize,
-        pub output: u8,
-        pub err_msg: String,
-    }
     
     //fn load_tests(path: String) -> Vec<(usize, usize, String)> {
     fn load_tests(path: String)  -> Result<(),Box<Error> >{
@@ -48,42 +46,20 @@ mod tests {
     fn example() -> Result<(), Box<Error>> {
         dbg!("example");
         let file = File::open("/home/noah/gits/rust_epi/tests/parity.tsv")?;
-
-        let mut data = String::new();
-        file.read_to_string(&mut data)?;
-
-        dbg!(data);
         //let file = "/home/noah/gits/rust_epi/tests/parity.tsv";
         //let mut rdr = csv::Reader::from_reader(file);
         //let mut rdr = csv::Reader::from_path(path);
-        let mut rdr = csv::ReaderBuilder::new().delimiter(b'\t').has_headers(false).from_reader(data.as_bytes());
-        //let mut iter = rdr.deserialize();
-        for result in rdr.deserialize() {
-            let record: Record = result?;
-        }
-        //if let Some(result) = iter.next() {
-        //    let record: Record = result.unwrap();
-        //}
-
-        //for result in rdr.deserialize() {
-        //for result in rdr.records() {
-        // for result: Record in rdr.deserialize() {
-            //dbg!(result);
-        //    let row: super::Record = result?;
-        //    dbg!(row);
-        //}
-        //let record: Row = result?;
-        //for result in rdr.deserialize() {
-        //    let record: Record = result?;
-        //    dbg!(record);
-            /*
-            let input = record.
-            let output = record.get(1).unwrap();
+        let mut rdr = csv::ReaderBuilder::new().delimiter(b'\t').has_headers(false).from_reader(file);
+        for result in rdr.records() {
+            let record = result?;
+            let input = record.get(0).unwrap().parse::<i64>().unwrap();
+            let output = record.get(1).unwrap().parse::<u8>().unwrap();
+            let msg = record.get(2).unwrap();
             dbg!(input);
             dbg!(output);
+            dbg!(msg);
             //println!("{:?}", record);
-            */
-        //}
+        }
         Ok(())
     }
 
