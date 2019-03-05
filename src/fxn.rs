bd503ff7e80144e06c8883ec57eedacbbba7b335
Noah Lidell <noahlidell@gmail.com>
2019-03-04 23:19:59 -0600

    wrote parity fxn, mvp'd test for parity

--- src/fxn.rs
+++ src/fxn.rs
@@ -1,3 +1,13 @@
-pub fn parity() -> i32 {
-    1
+pub fn parity(n: usize) -> usize {
+    let mut x = n;
+    let mut parity = 0;
+    loop {
+        if x == 0 {
+            break;
+        }
+        let y = x - 1;
+        x = x & y;
+        parity = parity ^ 1;
+    }
+    parity
 }
