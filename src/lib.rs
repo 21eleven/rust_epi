f19c5ee0c29088c31d3cd0a6369a553fc0af6e83
Noah Lidell <noah.lidell@gmail.com>
2019-03-04 23:21:10 -0600

    removed some unneded testing code

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,6 @@
 extern crate csv;
 
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-
-#[derive(Debug,Deserialize)]
-struct Record {
-    input: usize,
-    output: u8,
-    err_msg: String,
-}
-
-
 mod fxn;
-
 
 #[cfg(test)]
 mod tests {
@@ -35,33 +22,6 @@ mod tests {
         Ok(rows)
     }
 
-
-    #[test]
-    fn example() -> Result<(), Box<Error>> {
-        dbg!("example");
-        //let file = File::open("/home/noah/gits/rust_epi/tests/parity.tsv")?;
-        let file = File::open("./tests/parity.tsv")?;
-        //let file = "/home/noah/gits/rust_epi/tests/parity.tsv";
-        //let mut rdr = csv::Reader::from_reader(file);
-        //let mut rdr = csv::Reader::from_path(path);
-        let mut rdr = csv::ReaderBuilder::new().delimiter(b'\t').has_headers(false).from_reader(file);
-        for result in rdr.records() {
-            let record = result?;
-            let input = record.get(0).unwrap().parse::<i64>().unwrap();
-            let output = record.get(1).unwrap().parse::<u8>().unwrap();
-            let msg = record.get(2).unwrap();
-            dbg!(input);
-            dbg!(output);
-            dbg!(msg);
-            //println!("{:?}", record);
-        }
-        Ok(())
-    }
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
     #[test]
     fn parity() {
         let name = "parity".into();
